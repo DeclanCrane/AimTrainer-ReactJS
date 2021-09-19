8bb0db9162b9bd790a67eb33ff150646fbee44ae
Declan Crane <DeclanCrane10@gmail.com>
2021-09-19 17:24:05 -0300

    Finished game, Target needs refactoring

--- src/components/Target.js
+++ src/components/Target.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../Target.css';
 
-const Target = ({ containerRef }) => {
+const Target = ({ containerRef, updateScore }) => {
 
-    const targetLifeTime = 1;
+    // Time in seconds you have to click the target
+    const targetLifeTime = 0.8;
 
     // Timer handle
     let [timerConfig, setTimerConfig] = useState({timer: 0});
@@ -11,51 +12,86 @@ const Target = ({ containerRef }) => {
     // Target life time in seconds
     let [time, setTime] = useState(targetLifeTime);
 
+    // Target is alive
+    let [alive, setAlive] = useState(false);
+ 
     // Position and color of target
-    let [styling, setStyling] = useState({top: 0, left: 0});
+    let [styling, setStyling] = useState({top: 0, left: 0, background: 'green'});
+
+    const targetDivRef = useRef();
 
     useEffect(() => {
+        if(!alive) {
+            // Clear Timer
+            clearInterval(timerConfig.timer);
 
-    }, []);
+            // Spawn target randomly
+            const {top, left} = getRandomPosition(containerRef);
+            setStyling({top, left});
+
+            // Reset Target Timer
+            setTime(targetLifeTime);
+
+            setAlive(true);
+        }
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [alive]);
 
     useEffect(() => {
+        // When time runs out
         if(time.toFixed(1) == 0) {
             // Clear Timer
             clearInterval(timerConfig.timer);
 
-            // Reset Timer
-            setTime(targetLifeTime);
+            // Decrease game score
+            updateScore(-1);
 
-            console.log('Dead');
+            // Reset Target
+            setAlive(false);
         }
-
+        // When timer resets
         if(time === targetLifeTime) {
-            // Spawn target randomly
-            const {top, left} = getRandomPosition();
-            setStyling({top, left});
-
             // Start target timer
             startTimer();
-            
-            console.log('Spawned');
         }
+
+        /* Rewrite this better, shouldn't be hard coded conditionals... */
+        if(time.toFixed(1) == 0.4) {
+            setStyling({ top: styling.top, left: styling.left, background: 'yellow' });
+        }
+
+        if(time.toFixed(1) == 0.2) {
+            setStyling({ top: styling.top, left: styling.left, background: 'red' });
+        }
+
+        if(time.toFixed(1) == 0.8) {
+            setStyling({ top: styling.top, left: styling.left, background: 'green' });
+        }
+
+        //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [time])
 
     function startTimer() {
         timerConfig.timer = setInterval(() => {
-            console.log(time);
             setTime(time -= 0.1);
         }, 100);
     }
 
-    function getRandomPosition() {
-        const height = containerRef.current.clientHeight;
-        const width = containerRef.current.clientWidth;
+    // Gets a random position inside of the game container
+    function getRandomPosition(container) {
+        /* Get parent container dimensions */
+        let height = container.current.clientHeight;
+        let width = container.current.clientWidth;
 
-        /* Get a random position from inside the container bounds,
-        and add padding for target so it doesn't go off-screen */
-        const top = Math.random() * (0 - height + 180) * -1;
-        const left = Math.random() * (0 - width + 150) * -1;
+        /* Get target container dimensions and subtract from parent 
+        container dimensions so the target won't go out of screen bounds */
+        height -= targetDivRef.current.clientHeight + 200; // Need to find a better way to do this, it's hard-coded for now. Not working as expected
+        width -= targetDivRef.current.clientWidth;
+
+        /* Get a random position from inside the container bounds
+        for target so it doesn't go off-screen */
+        const top = Math.floor(Math.random() * (height - 0));
+        const left = Math.floor(Math.random() * (width - 0));
 
         return { top, left };
     }
@@ -64,10 +100,12 @@ const Target = ({ containerRef }) => {
         <div
              className='target-div' 
              style={{top: styling.top, left: styling.left}}
+             ref={targetDivRef}
+             onClick={() => { setAlive(false); updateScore(1); }}
             >
             {/* When target is clicked, setTime to 0 so target will respawn */}
-            <div onClick={() => setTime(0)} className='target' />
-            <h1 className='target-info'>{time.toFixed(1) <= 0 ? 0 : time.toFixed(1)}</h1>
+            <div className='target' style={{background: styling.background}} />
+            <h1 unselectable="on" className='target-info'>{time.toFixed(1) <= 0 ? 0 : time.toFixed(1)}</h1>
         </div>
     )
 }
--- src/components/TargetHandler.js
+++ src/components/TargetHandler.js
@@ -4,25 +4,82 @@ import '../TargetHandler.css';
 
 const TargetHandler = () => {
 
-    // Keeps track of the player's game score
+    // Time in seconds the countdown lasts
+    const countdownLength = 3;
+
+    // The length of time in seconds the game lasts
+    const gameLength = 30;
+
+    // Player's game score
     const [score, setScore] = useState(0);
+
+    // Time before game starts in seconds
+    let [gameStartTime, setGameStartTime] = useState(countdownLength);
+
+    // Time before game ends in seconds
+    let [gameTime, setGameTime] = useState(gameLength);
 
     const containerRef = useRef();
 
     useEffect(() => {
+        startGameStartTimer();
+
+        //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    function testFunction(callback) {
-        console.log('Clicked!');
-        if(typeof callback == "function") {
-            callback();
-        }
+    // Starts game-start countdown timer
+    function startGameStartTimer() {
+        const gameStartTimer = setInterval(() => {
+            setGameStartTime(gameStartTime -= 1);
+            if(gameStartTime <= 0) {
+                clearInterval(gameStartTimer);
+                startGameTimer();
+            }
+        }, 1000);
+    }
+
+    // Starts game timer
+    function startGameTimer() {
+        const gameTimer = setInterval(() => {
+            setGameTime(gameTime -= 1);
+            if(gameTime === 0) {
+                clearInterval(gameTimer);
+            }
+        }, 1000);
+    }
+
+    // Updates game score
+    function updateScore(number) {
+        setScore(score + number);
+    }
+
+    // Resets values and restarts game
+    function restartGame() {
+        // // Reset game values
+        // setScore(0);
+        // setGameStartTime(countdownLength);
+        // setGameTime(gameLength);
+
+        // // Restart game
+        // startGameStartTimer();
+
+        window.location.reload();
     }
 
     return (
         <>
+            <div className='game-info-div'>
+            <h1 className='game-info'>Score: {score}</h1>
+            <h1 className='game-info'>Time Left: {gameTime}</h1>
+            </div>
             <div ref={containerRef} className='container'>
-                <Target onTargetClicked={testFunction} containerRef={containerRef}/>
+                {/* If game start countdown isn't over, display countdown */}
+                {gameStartTime > 0 ? <h1 className='countdown'>{gameStartTime}</h1> : null}
+                {/* If game start countdown is over, and the game hasn't ended display targets */}
+                {gameStartTime === 0 && gameTime !== 0 ? <Target containerRef={containerRef} updateScore={updateScore}/> : null}
+
+                {/* If game ended show the restart button */}
+                {gameTime === 0 ? <button onClick={restartGame} className='restartBtn'>Restart</button> : null}
             </div>
         </>
     )
